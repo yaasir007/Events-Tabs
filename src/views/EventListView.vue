a530dc9256fb267dbfbf34061770e13b158d6ae5
yaasir cheekoory <yaasir1997@gmail.com>
2024-06-09 01:36:33 +0400

    install axios and get data

--- src/views/EventListView.vue
+++ src/views/EventListView.vue
@@ -1,43 +1,20 @@
 <script setup>
 import EventCard from '@/components/EventCard.vue';
-import { ref } from 'vue'
+import { ref, onMounted } from 'vue'
+import EventService from '@/services/EventService';
 
-const events = ref([
-  {
-    id: 8765432,
-    category: 'community gardening',
-    title: 'Plant a Row for the Hungry',
-    description: 'Help provide fresh produce for those in need by volunteering at this community garden event.',
-    location: 'Green Meadows Park',
-    date: '22 June 2024',
-    time: '9:00',
-    petsAllowed: true,
-    organizer: 'Green Thumbs Up'
-  },
-  {
-    id: 1234567,
-    category: 'coding workshop',
-    title: 'Build Your First Web App',
-    description: 'Learn the basics of web development in this beginner-friendly workshop.',
-    location: 'Tech Central',
-    date: '15 June 2024',
-    time: '10:00',
-    petsAllowed: false,
-    organizer: 'Code Ninjas'
-  },
-  {
-    id: 9876543,
-    category: 'board game night',
-    title: 'Dice, Drinks, and Dragons',
-    description: 'Bring your favorite board games and join us for an evening of fun and friendly competition.',
-    location: 'The Caffeinated Owl',
-    date: '29 June 2024',
-    time: '18:00',
-    petsAllowed: true,
-    organizer: 'The Board Game Bunch'
-  }
-])
-</script> 
+const events = ref(null)
+
+onMounted(() => {
+  EventService.getEvents()
+  .then((response) => {
+    events.value = response.data
+  })
+  .catch((error) => {
+    console.log(error)
+  })
+})
+</script>
 
 <template>
   <h1>Events for Good</h1>
